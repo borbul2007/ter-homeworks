ad6fce6150a9a6f2978abe7b3675977c9c92ea17
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-05 21:03:34 +0300

    Update

--- 03/src/count-vm.tf
+++ 03/src/count-vm.tf
@@ -1,20 +1,20 @@
 data "yandex_compute_image" "ubuntu" {
-  family = var.vm_yandex_compute_image_family
+  family = "ubuntu-2004-lts"
 }
 
 resource "yandex_compute_instance" "web" {
   count = 2
-  name  = "${var.vm_yandex_compute_instance_name}-${count.index}"
+  name  = "web-${count.index}"
   platform_id = var.vm_yandex_compute_instance_platform_id
   zone        = var.default_zone
   resources {
-    cores         = var.vm_yandex_compute_instance_resources_cores
-    memory        = var.vm_yandex_compute_instance_resources_memory
-    core_fraction = var.vm_yandex_compute_instance_resources_core_fraction
+    cores         = 1
+    memory        = 1
+    core_fraction = 5
   }
   boot_disk {
     initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.image_id
+      image_id = data.yandex_compute_image.ubuntu-2004-lts.image_id
     }
   }
   scheduling_policy {
@@ -22,7 +22,7 @@ resource "yandex_compute_instance" "web" {
   }
   network_interface {
     subnet_id          = yandex_vpc_subnet.develop.id
-    security_group_ids = "${yandex_vpc_security_group.example.id}"
+    security_group_ids = "example"
     nat                = true
   }
 
