d035ce866d225f118164817e11a59bdda23b46ab
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-06 10:00:11 +0300

    Update

---
+++ 03/src/for_each-vm.tf
@@ -0,0 +1,33 @@
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_compute_instance" "db" {
+for_each = 
+  name  = "web-${count.index}"
+  platform_id = "standard-v1"
+  zone        = var.default_zone
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id
+    security_group_ids = [ yandex_vpc_security_group.example.id ]
+    nat                = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+}
