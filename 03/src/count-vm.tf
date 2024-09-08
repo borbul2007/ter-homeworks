fe51de4e9b9295ed2bb70a4d27d1657ca3ded395
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-08 10:56:19 +0300

    Update

--- 03/src/count-vm.tf
+++ 03/src/count-vm.tf
@@ -1,6 +1,7 @@
 resource "yandex_compute_instance" "web" {
-  count = 2
-  name  = "web-${count.index}"
+  count       = 2
+  depends_on  = [yandex_compute_instance.db]
+  name        = "web-${count.index}"
   platform_id = "standard-v1"
   zone        = var.default_zone
   resources {
