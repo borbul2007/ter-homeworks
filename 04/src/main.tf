6eb39783207d9bf401073a303547f5fd2954431f
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-10 10:34:07 +0300

    Update

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -15,5 +15,42 @@ data template_file "userdata" {
   }
 }
 
-module.analytics_vm {source = ./}
-module.marketing_vm {source = ./}
+module analytics-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  instance_name  = "analytics-vm"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = { 
+    project = "analytics"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+}
+
+module "marketing-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  instance_name  = "marketing-vm"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = { 
+    project = "marketing"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+}
