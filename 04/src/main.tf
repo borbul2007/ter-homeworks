c10b3916426282c0bda1bc2c4f9e53ce7aa12066
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-10 10:41:42 +0300

    Upda

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -8,18 +8,18 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
-data template_file "userdata" {
-  template = file("${path.module}/cloud-init.yml")
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
   vars = {
     ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiVcfW8Wa/DxbBNzmQcwn7hJOj7ji9eoTpFakVnY/AI"
   }
 }
 
-module "analytics-vm" {
+module analytics-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   network_id     = yandex_vpc_network.develop.id
   subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
   instance_name  = "analytics-vm"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
@@ -39,7 +39,7 @@ module "marketing-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   network_id     = yandex_vpc_network.develop.id
   subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
   instance_name  = "marketing-vm"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
