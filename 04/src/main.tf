f59c594cd13894ef03b85901340e0be494c382f8
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-10 13:14:51 +0300

    Update

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -1,13 +1,3 @@
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
-}
-
 data "template_file" "cloudinit" {
   template = file("./cloud-init.yml")
   vars = {
@@ -15,42 +5,59 @@ data "template_file" "cloudinit" {
   }
 }
 
-module "analytics-vm" {
-  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+#создаем подсеть
+resource "yandex_vpc_subnet" "develop_a" {
+  name           = "develop-ru-central1-a"
+  zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.develop.id
-  subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop.id]
-  instance_name  = "analytics-vm"
-  instance_count = 1
+  v4_cidr_blocks = ["10.0.1.0/24"]
+}
+
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = "develop-ru-central1-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = ["10.0.2.0/24"]
+}
+
+module "analytics_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop" 
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a","ru-central1-b"]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id,yandex_vpc_subnet.develop_b.id]
+  instance_name  = "webs"
+  instance_count = 2
   image_family   = "ubuntu-2004-lts"
   public_ip      = true
 
   labels = { 
-    project = "analytics"
-  }
+    project = "acanalytics"
+     }
 
   metadata = {
-    user-data          = data.template_file.cloudinit.rendered
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
     serial-port-enable = 1
   }
 }
 
-module "marketing-vm" {
+module "marketing_vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "stage"
   network_id     = yandex_vpc_network.develop.id
   subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop.id]
-  instance_name  = "marketing-vm"
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  instance_name  = "web-stage"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
   public_ip      = true
 
   labels = { 
     project = "marketing"
-  }
+     }
 
   metadata = {
-    user-data          = data.template_file.cloudinit.rendered
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
     serial-port-enable = 1
   }
 }
