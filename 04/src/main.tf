ca928253d08476d0b0f224bfa75c1e6885318643
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-11 09:28:47 +0300

    Update

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -67,9 +67,9 @@ module "marketing_vm" {
   }
 }
 
-  module "vpc_dev" {
-    source   = ./vpc
-    env_name = "develop"
-    zone     = "ru-central1-a"
-    cidr     = "10.0.1.0/24"
-  }
+module "vpc_dev" {
+  source   = "./vpc"
+  env_name = "develop"
+  zone     = "ru-central1-a"
+  cidr     = "10.0.1.0/24"
+}
