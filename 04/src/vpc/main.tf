ed5eefd73de2c267eec713b37fb4a29c0ddbb326
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-12 15:39:30 +0300

    U

--- 04/src/vpc/main.tf
+++ 04/src/vpc/main.tf
@@ -7,5 +7,5 @@ resource "yandex_vpc_subnet" "vpc_subnet_dev" {
   name           = var.env_name
   zone           = var.zone
   network_id     = yandex_vpc_network.vpc_dev.id
-  v4_cidr_blocks = var.cidr
+  v4_cidr_blocks = [var.cidr]
 }
