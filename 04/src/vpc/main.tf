fbb99ddcdbd6b9d769d1b2d5823e964ca454b978
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-12 10:11:42 +0300

    Update

--- 04/src/vpc/main.tf
+++ 04/src/vpc/main.tf
@@ -1,9 +1,9 @@
 
-resource "yandex_vpc_network" "vpc_dev" {
+resource "yandex-cloud/yandex_vpc_network" "vpc_dev" {
   name = "dev"
 }
 
-resource "yandex_vpc_subnet" "vpc_subnet_dev" {
+resource "yandex-cloud/yandex_vpc_subnet" "vpc_subnet_dev" {
   name           = var.env_name
   zone           = var.zone
   network_id     = yandex_vpc_network.vpc_dev.id
