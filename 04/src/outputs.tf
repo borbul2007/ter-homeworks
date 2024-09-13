5ce160d9935f7b89889c899a2db831f90f5394e3
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-13 10:24:25 +0300

    Update

--- 04/src/outputs.tf
+++ 04/src/outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_dev" {
+  value = module.vpc_dev.vpc_dev
+}
+
+output "vpc_subnet_dev" {
   value = module.vpc_dev.vpc_subnet_dev
 }
--- 04/src/vpc/outputs.tf
+++ 04/src/vpc/outputs.tf
@@ -1,3 +1,7 @@
+output "vpc_dev" {
+  value = yandex_vpc.vpc_dev
+}
+
 output "vpc_subnet_dev" {
   value = yandex_vpc_subnet.vpc_subnet_dev
 }
