6f54b97cccd604250ab520cc6c55d923a5319ee7
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-13 10:45:57 +0300

    Update

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -49,9 +49,9 @@ module "analytics_vm" {
 module "marketing_vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "stage"
-  network_id     = yandex_vpc_network.develop.id
-  subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  network_id     = module.vpc_dev.vpc_subnet_dev.network_id
+  subnet_zones   = [module.vpc_dev.vpc_subnet_dev.zone]
+  subnet_ids     = [module.vpc_dev.vpc_subnet_dev.id]
   instance_name  = "web-stage"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
--- 04/src/outputs.tf
+++ 04/src/outputs.tf
@@ -1,7 +1,3 @@
-output "vpc_dev" {
-  value = module.vpc_dev.vpc_dev
-}
-
 output "vpc_subnet_dev" {
   value = module.vpc_dev.vpc_subnet_dev
 }
