ebfd0986d3ddfaa1f82e25e8a487e066908ea266
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-16 10:52:05 +0300

    Update

--- 04/src/providers.tf
+++ 04/src/providers.tf
@@ -7,16 +7,18 @@ terraform {
   required_version = "~>1.8.4"
 
   backend "s3" {
-    endpoint = { s3 = "storage.yandexcloud.net" }
+    endpoints = { s3 = "storage.yandexcloud.net" }
     bucket = "tfstate-develop-1"
     region = "ru-central1"
     key = "terraform.tfstate"
   
-    skip_region_validation = true
+    skip_region_validation      = true
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   
-    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1guau0af4j7qkg1484e/etn42544b31a743m6357"
-    dynamodb_table = "tfstate-lock-develop"
+#    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1guau0af4j7qkg1484e/etn42544b31a743m6357"
+#    dynamodb_table = "tfstate-lock-develop"
   }
 }
 
