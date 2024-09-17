28201bb83f68ee847488b8a1f558111343b305e9
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-17 10:19:25 +0300

    Update

--- 05/variables.tf
+++ 05/variables.tf
@@ -3,7 +3,7 @@ variable "ip`_addr" {
   default     = "192.168.0.1"
   description = "ip-адрес"
   validation {
-    condition     = can(cidrhost(var.ip_addr))
+    condition     = cidrhost(var.ip_addr)
     error_message = "Must be a valid IPv4 address."
   }
 }
