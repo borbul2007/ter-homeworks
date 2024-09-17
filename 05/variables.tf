85784d9cfd03e413242e9e2ac633ab9b1f4930e9
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-17 18:19:34 +0300

    Update

--- 05/variables.tf
+++ 05/variables.tf
@@ -7,3 +7,13 @@ variable "ip_addr" {
     error_message = "Must be a valid IPv4 addr"
   }
 }
+
+variable "ip_addr_list {
+  type        = list(string)
+  default     = ["192.168.0.1", "192.168.0.2"]
+  description = "список ip-адресов"
+  validation {
+    condition     = alltrue([for ip_addr in var.ip_addr_list : can(cidrhost(var.ip_addr, 0))])
+    error_message = "Must be a valid IPv4 addr"
+  }
+}
