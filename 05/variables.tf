0da6321270dd44277a25c3621a3f2ed5763432c8
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-17 18:45:46 +0300

    Update

--- 05/variables.tf
+++ 05/variables.tf
@@ -1,6 +1,7 @@
 variable "ip_addr" {
   type        = string
-  default     = "192.168.0.1"
+#  default     = "192.168.0.1"
+  default     = "1920.1680.0.1"
   description = "ip-адрес"
   validation {
     condition     = can(cidrhost("${var.ip_addr}/32", 0))
@@ -10,7 +11,8 @@ variable "ip_addr" {
 
 variable "ip_addr_list" {
   type        = list(string)
-  default     = ["192.168.0.1", "192.168.0.2"]
+#  default     = ["192.168.0.1", "1.1.1.1", "127.0.0.1"]
+  default     = ["192.168.0.1", "1.1.1.1", "1270.0.0.1"]
   description = "список ip-адресов"
   validation {
     condition     = alltrue([for ip_addr in var.ip_addr_list : can(cidrhost("${ip_addr}/32", 0))])
